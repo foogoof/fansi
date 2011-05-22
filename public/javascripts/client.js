c57728ff6a673fdb8e7082ac602b75888b6b78ff
seth-schroeder <foogoof@gmail.com>
2011-05-22 08:56:26 -0400

    add jquery

---
+++ public/javascripts/client.js
@@ -0,0 +1,20 @@
+// var cur_x = 0, cur_y = 0;
+
+// var cur_cell = function() {
+//     var idstr = "#" + cur_x + "_" + cur_y;
+//     var cell = $(idstr);
+//     if (!cell) { console.log("no cell for " + idstr); }
+//     return cell;
+// };
+
+// var go_right = function() {
+//     cur_x++;
+// };
+
+// var on_raw_data = function(data) {
+//     for (var i = 0; i < data.length; i++) {
+//         var cc = cur_cell();
+//         cc.text(data[i]);
+//         go_right();
+//     }
+// };
