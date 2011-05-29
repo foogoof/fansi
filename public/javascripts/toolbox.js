1bec5c633000b55485ccd9b023bc457a6011c108
seth-schroeder <foogoof@gmail.com>
2011-05-28 22:04:27 -0400

    add header

--- public/javascripts/toolbox.js
+++ public/javascripts/toolbox.js
@@ -1,3 +1,21 @@
+// fansi is a javascript project to convert control sequences into events
+//
+// Copyright (C) 2011  Seth Schroeder <foogoof@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 var s = require('./s'),
     _ = require('underscore');
 
